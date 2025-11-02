12280fbe1aa860b5db7dfb148facd0d0790afcc8
Danton Klein <dantonmklein@gmail.com>
2025-11-02 14:41:40 -0500

    glerk

--- tb/reciprocal_tb.sv
+++ tb/reciprocal_tb.sv
@@ -73,7 +73,7 @@ module reciprocal_tb #(
     logic valid_data_out;
    // logic normalized_mantissa_lsb, normalized_guard, normalized_round, normalized_sticky, round_up;
 
-    fp_reciprocal_pipeline DUT (.*);
+    fp_reciprocal_pipeline2 DUT (.*);
 
     
     initial begin : generate_clock
